403130eb34acdd00a8cc054151acba562a86fcfd
Dab <luisdanielugartechavez@gmail.com>
2024-05-08 21:25:12 -0600

    IUser

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,32 +1,23 @@
-const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-const admin = require('./../config/firebase')
+const User = require('../models/User')
 
 const loginUser = async (req, res) => {
   try {
     const { email, password } = req.body
-
-    const userDoc = await admin.firestore().collection('users').doc(email).get()
-
-    if (!userDoc.exists) {
+    const userDoc = await User.findByEmail(email)
+    if (!userDoc) {
       return res.status(404).json({
         message: 'User not found'
       })
     }
-
-    const userData = userDoc.data()
-
-    const isValidPass = await bcrypt.compare(password, userData.password)
-    
+    const isValidPass = await userDoc.verifyPassword(password)
     if(!isValidPass) {
       return res.status(401).json({
         message: 'Invalid Credentials'
       })
     }
-
-    const token = jwt.sign({ email: userData.email }, process.env.SECRET, { expiresIn: '1h' })
+    const token = jwt.sign({ email: userDoc.email }, process.env.SECRET, { expiresIn: '1h' })
     res.status(200).json({ token })
-
   } catch (error) {
     res.status(500).json({
       message: 'Internal Server Error'
@@ -35,23 +26,18 @@ const loginUser = async (req, res) => {
 }
 
 const registerUser = async (req, res) => {
-  console.log('@@@ body => ', req.body)
-
   try {
     const { email, password } = req.body
-    console.log('@@@ body => ', email)
-    console.log('@@@ pass => ', password)
-
-    const hashed = await bcrypt.hash(password, 10)
-    console.log('@@@ hashed => ', hashed)
-
-    await admin.firestore().collection('users').doc(email).set({
-      email: email,
-      password: hashed
-    })
-
+    const existingUser = await User.findByEmail(email)
+    if (existingUser) {
+      return res.status(400).json({
+        message: 'User already exists'
+      })
+    }
+    const newUser = await User.createUser(email, password)
     res.status(201).json({
-      message: 'User registered successfully'
+      message: 'User registered successfully',
+      user: newUser
     })
 
   } catch (error) {
